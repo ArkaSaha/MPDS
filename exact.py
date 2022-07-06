b2e793292500c5a9fe6cc8c6ac699a17caa047f5
ArkaSaha <arkapravasaha.18@gmail.com>
2022-07-06 23:20:20 +0800

    Exact Algorithms

---
+++ exact.py
@@ -0,0 +1,26 @@
+import sys
+from os import system
+
+graph = sys.argv[1]
+output = sys.argv[2]
+theta = int(sys.argv[3])
+k = int(sys.argv[4])
+notion = sys.argv[5]
+intake = lambda msg : input(msg) if sys.version_info[0] >= 3 else raw_input(msg)
+
+if notion == "edge":
+	executable = "edge/exact_edge"
+	system("rm -f {ex} && g++ -O3 -lpthread -std=c++11 -o {ex} {ex}.cpp".format(ex = executable))
+	system("{} {} {} {} {}".format(executable, graph, theta, k, output))
+elif notion == "clique":
+	h = intake("Value of h (3/4/5): ")
+	executable = "clique/exact_clique"
+	system("rm -f {ex} && g++ -O3 -lpthread -std=c++11 -o {ex} {ex}.cpp".format(ex = executable))
+	system("{} {} {} {} {} {}".format(executable, graph, theta, k, h, output))
+elif notion == "pattern":
+	psi = intake("Pattern (2-star/3-star/c3-star/diamond): ")
+	executable = "pattern/exact_pattern"
+	system("rm -f {ex} && g++ -O3 -lpthread -std=c++11 -o {ex} {ex}.cpp".format(ex = executable))
+	system("{} {} {} {} {} {}".format(executable, graph, theta, k, psi, output))
+else:
+	assert False, "Invalid notion of density"
